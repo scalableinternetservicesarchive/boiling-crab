013af8f45973be09c5e526b5cfc13befa495c107
Ziqiang Shi <ziqiang.shi0804@gmail.com>
2015-10-22 14:17:37 -0700

    add authentication error prompt

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,17 @@
 class UsersController < ApplicationController
   def new
-    @user = User.new
+      @user = User.new
   end
 
   def create 
       @user = User.new(user_params) 
+
       if @user.save 
         session[:user_id] = @user.id 
         redirect_to '/' 
       else 
-        redirect_to '/signup' 
+        # redirect_to '/signup' 
+        render :action => 'new'
       end 
   end
 
