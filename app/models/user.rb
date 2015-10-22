c947cfca44fcf6f7431d153240a15ac62135f86d
Ziqiang Shi <ziqiang.shi0804@gmail.com>
2015-10-22 01:37:39 -0700

    email uniquness

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ActiveRecord::Base
     has_secure_password
 
     validates :first_name, :last_name, :email, :password_digest, :presence => true
+    validates :email, uniqueness: true
 end
