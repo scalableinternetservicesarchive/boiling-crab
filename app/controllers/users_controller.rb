cd78085bcbaf79fbb5353f0241fcfc10d9bbeafa
Ziqiang Shi <ziqiang.shi0804@gmail.com>
2015-11-04 15:04:49 -0800

    profile post order

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @posts = @user.posts.all
+    @posts = @user.posts.all.order(created_at: :desc)
   end
 
   def following
