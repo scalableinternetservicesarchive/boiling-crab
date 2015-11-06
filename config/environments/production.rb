dfc470390a1c464ed4046fbe8aec6a84e2abb3a8
Kai Wang <WK@vpn-128-97-245-91.host.ucla.edu>
2015-11-05 23:08:22 -0800

    by pass css precompile error, hint from York

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,8 +25,10 @@ Rails.application.configure do
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  #config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = NoCompression.new
   # config.assets.css_compressor = :sass
+  config.assets.css_compressor = NoCompression.new
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
