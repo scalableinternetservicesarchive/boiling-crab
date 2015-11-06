eaaef96627be1623554d9a55655a2b1dd336a2f2
Shijian <wind1689@gmail.com>
2015-11-06 02:53:45 -0800

    Add image and complete seed

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,5 @@ class User < ActiveRecord::Base
     
     validates :first_name, :last_name, :email, :password_digest, :presence => true
     validates :email, uniqueness: true
+    validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,30 +5,47 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-u1 = User.create(first_name: 'a', last_name: 'a', email: 'a@a', password: 'a', password_confirmation: 'a', nFollowing: 1, nFollower: 1, nPost: 2)
-u2 = User.create(first_name: 'b', last_name: 'b', email: 'b@b', password: 'b', password_confirmation: 'b', nFollowing: 1, nFollower: 1, nPost: 2)
-u3 = User.create(first_name: 'c', last_name: 'c', email: 'c@c', password: 'c', password_confirmation: 'c', nFollowing: 1, nFollower: 1, nPost: 2)
-u4 = User.create(first_name: 'd', last_name: 'd', email: 'd@d', password: 'd', password_confirmation: 'd', nFollowing: 1, nFollower: 1, nPost: 2)
+u1 = User.create(first_name: 'Ziqiang', last_name: 'Shi', email: 'a@a', password: 'a', password_confirmation: 'a',
+	nFollowing: 1, nFollower: 1, nPost: 2, image: 'http://ccgexpo.cn/ckeditor_assets/pictures/000/000/253/content_image007.jpg')
+u2 = User.create(first_name: 'Shijian', last_name: 'Zheng', email: 'b@b', password: 'b', password_confirmation: 'b',
+	nFollowing: 1, nFollower: 1, nPost: 2, image: 'http://img9.dzdwl.com/img/11759906_152609217127_2.jpg')
+u3 = User.create(first_name: 'Kai', last_name: 'Wang', email: 'c@c', password: 'c', password_confirmation: 'c',
+	nFollowing: 1, nFollower: 1, nPost: 2, image: 'http://wyxgroup-img.stor.appsina.com/9e2acb4f9f404a4b088a1d7c81583d54.jpg')
+u4 = User.create(first_name: 'Yu', last_name: 'Xie', email: 'd@d', password: 'd', password_confirmation: 'd',
+	nFollowing: 1, nFollower: 1, nPost: 2, image: 'http://www.80ktv.net/cs/uploads/allimg/130926/103G32L6-2.jpg')
 
-Friendship.create(user_id: 1, friend_id: 2)
-Friendship.create(user_id: 2, friend_id: 3)
-Friendship.create(user_id: 3, friend_id: 4)
-Friendship.create(user_id: 4, friend_id: 1)
+Friendship.create(user_id: u1.id, friend_id: u2.id)
+Friendship.create(user_id: u1.id, friend_id: u3.id)
+Friendship.create(user_id: u2.id, friend_id: u3.id)
+Friendship.create(user_id: u2.id, friend_id: u4.id)
+Friendship.create(user_id: u3.id, friend_id: u4.id)
+Friendship.create(user_id: u3.id, friend_id: u1.id)
+Friendship.create(user_id: u4.id, friend_id: u1.id)
+Friendship.create(user_id: u4.id, friend_id: u2.id)
 
-p1 = Post.create(title: 'aa', description: 'aa', price: 1, user_id: u1.id, sell_to: -1)
-p2 = Post.create(title: 'aaa', description: 'aaa', price: 1, user_id: u1.id, sell_to: -1)
-p3 = Post.create(title: 'bb', description: 'bb', price: 1, user_id: u2.id, sell_to: -1)
-p4 = Post.create(title: 'bbb', description: 'bbb', price: 1, user_id: u2.id, sell_to: -1)
-p5 = Post.create(title: 'cc', description: 'cc', price: 1, user_id: u3.id, sell_to: -1)
-p6 = Post.create(title: 'ccc', description: 'ccc', price: 1, user_id: u3.id, sell_to: -1)
-p7 = Post.create(title: 'dd', description: 'dd', price: 1, user_id: u4.id, sell_to: -1)
-p8 = Post.create(title: 'ddd', description: 'ddd', price: 1, user_id: u4.id, sell_to: -1)
+p1 = Post.create(title: 'chair', description: 'This is the best second-handed chair!',
+	price: 10, user_id: u1.id, sell_to: -1, image: 'https://martinclemens.files.wordpress.com/2009/03/magnussen-edge-wood-desk-chair-chair_0_0.jpg')
+p2 = Post.create(title: 'table', description: 'This is the best second-handed table!',
+	price: 20, user_id: u1.id, sell_to: -1, image: 'http://st.hzcdn.com/simgs/74e1b7e70245dc09_4-5066/rustic-coffee-tables.jpg')
+p3 = Post.create(title: 'lamp', description: 'This is the best second-handed lamp!',
+	price: 15, user_id: u2.id, sell_to: -1, image: 'http://www.incrediblethings.com/wp-content/uploads/2012/06/dog-pooping-lamp-1.jpg')
+p4 = Post.create(title: 'mattress', description: 'This is the most soft second-handed mattress!',
+	price: 80, user_id: u2.id, sell_to: -1, image: 'http://maximmattress.com/storage/mattress3.jpg')
+p5 = Post.create(title: 'bike', description: 'This is the coolest second-handed bike!',
+	price: 100, user_id: u3.id, sell_to: -1, image: 'http://uncrate.com/p/2014/03/rungu-juggernaut-bike.jpg')
+p6 = Post.create(title: 'car', description: 'This is the best second-handed car!',
+	price: 1000, user_id: u3.id, sell_to: -1, image: 'http://blog.automart.co.za/wp-content/uploads/2014/07/Second-hand-cars-for-sale-1024x768.jpg')
+p7 = Post.create(title: 'superman', description: 'This is the most handsome second-handed superman!',
+	price: 8888, user_id: u4.id, sell_to: -1, image: 'http://cdn.blogosfere.it/nuvoleparlanti/images/Superman-Returns-Brandon-Routh.jpeg')
+p8 = Post.create(title: 'iphone', description: 'This is broken second-handed iphone!',
+	price: 50, user_id: u4.id, sell_to: -1, image: 'http://www.techlicious.com/images/phones/broken-iphone-editorial-only-510px.jpg')
 
-Comment.create(commender_id: 1, body: 'aaaaaaaaa', post_id: 1)
-Comment.create(commender_id: 4, body: 'ddddddddd', post_id: 1)
-Comment.create(commender_id: 2, body: 'bbbbbbbbb', post_id: 3)
-Comment.create(commender_id: 1, body: 'aaaaaaaaa', post_id: 3)
-Comment.create(commender_id: 3, body: 'ccccccccc', post_id: 5)
-Comment.create(commender_id: 2, body: 'bbbbbbbbb', post_id: 5)
-Comment.create(commender_id: 4, body: 'ddddddddd', post_id: 7)
-Comment.create(commender_id: 3, body: 'ccccccccc', post_id: 7)
+
+Comment.create(commender_id: 1, body: 'Buy the chair!', post_id: p1.id)
+Comment.create(commender_id: 4, body: 'The chair looks so old.', post_id: p1.id)
+Comment.create(commender_id: 2, body: 'Come buy my lamp!', post_id: p3.id)
+Comment.create(commender_id: 1, body: 'What a ugly lamp.', post_id: p3.id)
+Comment.create(commender_id: 3, body: 'My bike is so cool!', post_id: p5.id)
+Comment.create(commender_id: 2, body: 'I like it.', post_id: p5.id)
+Comment.create(commender_id: 4, body: 'It still works even it is broken.', post_id: p7.id)
+Comment.create(commender_id: 3, body: 'Keep it for me.', post_id: p7.id)
