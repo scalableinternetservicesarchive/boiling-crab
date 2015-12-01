af1ff515a9cdfbcaf82ff2614fdd968506d20618
ZHENGShijian <James@wifi-131-179-58-56.host.ucla.edu>
2015-12-01 13:24:54 -0800

    Add Pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,7 +29,8 @@ class UsersController < ApplicationController
 
   def feed
     feed_users_ids = current_user.friends.pluck(:id)
-    @feed_posts = Post.where(user_id: feed_users_ids, sell_to: -1).order(updated_at: :desc)
+    @feed_posts = Post.where(user_id: feed_users_ids, sell_to: -1)
+      .paginate(:page => params[:page], :per_page => 10).order(updated_at: :desc)
     @user = current_user
     @comment = Comment.new
   end
